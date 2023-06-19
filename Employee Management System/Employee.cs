9c9a18e37e930d61931b6d3e3b4ce45fe1989e05
Hossam-ELSherbiny <ho.elsherbiny@gmail.com>
2023-06-19 18:50:31 +0300

    Add project files.

---
+++ Employee Management System/Employee.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Employee_Management_System
+{
+    public class Employee
+    {
+        // Private Fields
+        private readonly string name;
+        private string address;
+        private double salary;
+        private string department;
+        private readonly int id;
+
+        // Constructos initialization..
+        public Employee(string employeeName, string address, double salary, string department, int id)
+        {
+            this.name = employeeName;
+            this.address = address;
+            this.salary = salary;
+            this.department = department;
+            this.id = id;   
+        }
+
+        // Method to add..
+        public void UpdateAddress(string newAddress)
+        {
+            address = newAddress;
+            Console.WriteLine("Address Updated.");
+        }
+
+        public void UpdateSalary(double newaSalary)
+        {
+            this.salary = newaSalary;
+            Console.WriteLine("Salary Updated Successfuly");
+        }
+
+        public void UpdateDepartment(string newDepartment)
+        {
+            department = newDepartment;
+            Console.WriteLine("Department Updated Successfuly");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Employee Data Deleted Successfuly");
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Employee Id: " + id);
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Address: " + address);
+            Console.WriteLine("Salary: " + salary);
+            Console.WriteLine("Department: " + department);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Employee Management System/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Employee_Management_System;
+
+// Create a list to store employee objects..
+List<Employee> employees = new List<Employee>();
+
+//  Add some employee to the list..
+employees.Add(new Employee("Hossam", "Istanbul", 25000.0, "No: 26", 10));
+employees.Add(new Employee("Muhammed", "Zonguldak", 25000.0, "No: 12", 2));
+employees.Add(new Employee("Hasan", "Akara", 44000.0, "No: 54", 4));
+employees.Add(new Employee("Hamza", "Iskendarun", 54000.0, "No: 5", 1));
+employees.Add(new Employee("Ali", "Isparta", 21000.0, "No:2", 6));
+
+// Display the information for each employee in the list.. 
+foreach(Employee employee in employees)
+{
+    employee.Display();
+}
+
+employees[1].UpdateSalary(72000.0);
+employees[2].UpdateAddress("Turkey");
+employees[0].UpdateDepartment("No: 27");
+
+// Display the information for each employee in the list after the updates
+foreach (Employee employee in employees)
+{
+    employee.Display();
+}
+
+employees[2].Delete();
+
+// Remove the employee object from the list
+employees.RemoveAt(0);
+
+// Display the information for each employee in the list after the deletion
+foreach (Employee employee in employees)
+{
+    employee.Display();
+}
